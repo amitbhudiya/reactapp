cc9f5a4969ecb8c2ebb87bc4a30e9e89aaddf0c2
amitbhudiya <59469748+amitbhudiya@users.noreply.github.com>
2021-12-18 22:19:15 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            this is about components
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet quod ipsum maxime? Obcaecati, ad sint maxime expedita cum nobis ab blanditiis illum perspiciatis quo dolores fugit voluptates tenetur sapiente adipisci temporibus ea! Quia, quas?
+            </p>
+        </div>
+    )
+}
